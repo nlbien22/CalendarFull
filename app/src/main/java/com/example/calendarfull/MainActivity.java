e1c035d060a0912465660bb87bf091c38d5063f9
nlbien.22 <nlbien.22@gmail.com>
2022-08-30 15:54:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/calendarfull/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.calendarfull;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+/**
+ * The launcher activity of the sample app. It contains the links to visit all the example screens.
+ * Created by Raquib-ul-Alam Kanak on 7/21/2014.
+ * Website: http://alamkanak.github.io
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonBasic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BasicActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.buttonAsynchronous).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AsynchronousActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
